895f408e0586e2363c57a5aa9447790fc0d1574c
airydev001 <airydev585@gmail.com>
2024-07-17 08:00:03 +0100

    changes to the app which is now working perfectly

--- lib/utils/app-colors.dart
+++ lib/utils/app-colors.dart
@@ -8,6 +8,6 @@ const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
 
-const primaryColor = Colors.black;
+const primaryColor = Colors.white;
 
 const secondaryColor = Colors.grey;
